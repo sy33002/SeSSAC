49af40f0a33a971cc1f73f8668fc320786c45497
kim_jihyung <sy33002@naver.com>
2023-08-04 17:46:22 +0900

    프로젝트 중간 업로드

---
+++ projeoct/mysite.js
@@ -0,0 +1,9 @@
+// 첫페이지
+const carouselExample = document.querySelector('#carouselExample');
+console.log(carouselExample);
+carouselExample.setAttribute('class', 'shadow-sm p-3 mb-5 bg-body-tertiary rounded');
+
+document.addEventListener("DOMContentLoaded", function() {
+    const element = document.querySelector("div");
+    element.classList.add("animate");
+});
