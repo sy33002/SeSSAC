61fc53f697d1383938b4e449871a93760d79230a
kim_jihyung <sy33002@naver.com>
2023-08-04 18:51:32 +0900

    프로젝트완료

--- projeoct/mysite.js
+++ projeoct/mysite.js
@@ -1,9 +1,15 @@
-// 첫페이지
-const carouselExample = document.querySelector('#carouselExample');
-console.log(carouselExample);
-carouselExample.setAttribute('class', 'shadow-sm p-3 mb-5 bg-body-tertiary rounded');
+// 스크롤 이벤트 리스너 등록
+window.addEventListener('scroll', function () {
+  // 요소가 뷰포트 안에 들어왔는지 체크하여 애니메이션을 주고자 하는 요소 선택
+  const elements = document.querySelectorAll('.point');
 
-document.addEventListener("DOMContentLoaded", function() {
-    const element = document.querySelector("div");
-    element.classList.add("animate");
+  elements.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight) {
+      // 뷰포트 안에 요소가 들어왔을 때 애니메이션을 주는 클래스 추가
+      element.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-1s');
+    }
+  });
 });
