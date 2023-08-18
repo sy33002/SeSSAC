a319f401c041e61e86760a61f001fa9b34c9c567
kim_jihyung <sy33002@naver.com>
2023-08-18 19:05:05 +0900

    8_18 수업 종료

---
+++ 12-dynamic-form/app2.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+app.set('/views', 'views');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.render('index2');
+});
+
+app.get('/axios', (req, res) => {
+    res.send(req.query);
+    console.log(req.query);
+});
+
+app.post('/post', (req, res) => {
+    res.send(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log(`${PORT} is open!`);
+});
