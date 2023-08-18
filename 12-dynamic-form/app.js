8aecab92f02e66fd43a2d322f05a284cb8284eba
kim_jihyung <sy33002@naver.com>
2023-08-18 14:43:23 +0900

    8_18수업 중간 업로드

---
+++ 12-dynamic-form/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+app.set('/views', 'views');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.render('dynamic');
+});
+
+app.get('/ajax', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+});
+
+app.post('/ajax', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log(`${PORT} is open!`);
+});
