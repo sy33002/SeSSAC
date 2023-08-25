14544f5758de607802a05c54be05648f24a47231
kim_jihyung <sy33002@naver.com>
2023-08-25 18:47:14 +0900

    8_25수업 종료

--- 14-mysql/practice.sql
+++ 14-mysql/practice.sql
@@ -141,16 +141,21 @@ delete from orders2 where order_date >= '2022-02-17';
 -- 책 ID: 1
 -- 고객 이름: Sarah Johnson
 -- 주문일자 : 2022-02-18
+insert into orders2 VALUES (4, 1, 'Sarah Johnson', '2022-02-18');
 
 -- 7. order_id가 1인 주문의 고객 이름을 Jack Smith로 업데이트하는 SQL 문을 작성합니다.
+update orders2 set customer_name = 'Jack Smith' where order_id = 1;
 
 -- 8. 다음 세부 정보와 함께 새 작성자를 삽입하는 SQL 문을 작성합니다.
 -- 저자 ID: 4
 -- 이름: agatha
 -- 성: christie
 -- 이메일: agatha.christie@example.com
+insert into authors VALUES(4, 'agatha', 'christie', 'agatha.christie@example.com');
 
 -- 9. author_id 2인 작성자의 성을 Martinez로 업데이트하는 SQL 문을 작성합니다.
+update authors set last_name = 'Martinez' where author_id = 2;
 
 -- 10. author_id 3인 저자가 쓴 모든 책을 삭제하는 SQL 문을 작성합니다.
+delete from books where author_id = 3;
 
