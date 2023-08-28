d18f5e0c996799ae6dcf375fe5cf192c9fd27bf6
kim_jihyung <sy33002@naver.com>
2023-08-28 18:30:00 +0900

    8_28수업종료

--- 16-mvc-mysql/controller/Cvisitor.js
+++ 16-mvc-mysql/controller/Cvisitor.js
@@ -8,14 +8,24 @@ exports.main = (req,res) => {
 exports.visitor = (req,res) => {
     // model에서 getVisitors는 인자로 콜백함수를 받고 있는 과정을 넣어주는 중
     Visitor.getVisitors((result) => {
-        res.render('visitor', {data: result})
+        res.render('visitor', {data: result});
     });
 };
 
 exports.postVisitor = (req, res) => {
+    console.log('req.body = ', req.body);
     Visitor.postVisitor(req.body, (insertId) => {
-        console.log(req.body);
         const { id, name, comment } = req.body;
         res.send({ id, name, comment });
     });
 };
+
+exports.deleteVisitor = (req, res) => {
+    console.log(req.body); // { id: xx }
+    const { id } = req.body;
+  
+    Visitor.deleteVisitor(id, (result) => {
+      console.log('controller >>', result); // true
+      res.send(result); // res.send(true)
+    });
+  };
--- 16-mvc-mysql/routes/index.js
+++ 16-mvc-mysql/routes/index.js
@@ -6,6 +6,7 @@ const controller = require('../controller/Cvisitor');
 router.get('/', controller.main);
 router.get('/visitors', controller.visitor);
 router.post('/visitor', controller.postVisitor);
+router.delete('/visitor', controller.deleteVisitor);
 
 module.exports = router;
 
