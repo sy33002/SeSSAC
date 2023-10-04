3e00c9accee05fde4a56f7c9e5de936ab5e9f8f4
kim_jihyung <sy33002@naver.com>
2023-10-04 17:33:12 +0900

    study: 실습 진행 중

--- 26_map-filter/component-practice/src/ex/Event_ex.js
+++ 26_map-filter/component-practice/src/ex/Event_ex.js
@@ -1,35 +1,33 @@
 import React, { useState } from 'react'
 
 export default function Event_ex() {
-  const [alphabet, setAlphabet] = useState([
+  const [user, setUser] = useState([
       {
-        id: '코드',
-        email: '',
+        id: 1,
+        name: '코디',
+        email: 'codi@gmail.com',
       },
       {
         id: 2,
-        alpha: 'p',
+        name: '윤소희',
+        email: 'yoonsohee@gmail.com',
       },
     ]);
-  const [inputAlpha, setInputAlpha] = useState('');
-  const addAlpha = () => {
-    // [퀴즈] input이 빈값이라면 alphabet 상태가 변경되지 않도록 하기
-    if (!inputAlpha) {
+  const [inputName, setInputName] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
+  const addUser = () => {
+    if (!inputName || !inputEmail) {
       alert('빈값입니다!')
     } else {
-      const newAlpha = alphabet.concat({id: alphabet.length+1, alpha: inputAlpha});
-      setAlphabet(newAlpha);
-      setInputAlpha('');
+      const newUser = user.concat({id: user.length+1, name: inputName, email: inputEmail});
+      setUser(newUser);
+      setInputName('');
+      setInputEmail('');
     }
   };
-  const deleteAlpha = (targetId) => {
-    const newAlpha = alphabet.filter((alpha) => alpha.id !== targetId);
-    setAlphabet(newAlpha);
-  };
-  // [퀴즈] input에서 enter 키 누르면 추가 되도록!
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      return addAlpha
+      return addUser
     }
   }
   return (
@@ -37,14 +35,14 @@ export default function Event_ex() {
       <input 
         type="text" 
         placeholder='알파벳 입력' 
-        value={inputAlpha} 
+        value={inputname} 
         onChange={(e) => {
           setInputAlpha(e.target.value);
       }} 
       />
-      <button onClick= {addAlpha}>ADD</button>
+      <button onClick= {addUser}>ADD</button>
       <ol>
-        {alphabet.map((value) => {
+        {user.map((value) => {
           return  <li key={value.id} onDoubleClick={() => deleteAlpha(value.id)} onKeyDown={handleKeyDown}>{value.alpha}</li>
         })}
       </ol>
