aa7aa7d5aa1811dd5a8ab90f891501f904f66124
kim_jihyung <sy33002@naver.com>
2023-10-18 17:39:11 +0900

    study: typescript 생성

--- 31_redux/src/Practice.js
+++ 31_redux/src/Practice.js
@@ -1,29 +1,24 @@
 import { useState } from 'react';
-import { MoneyContainer } from './containers/BoxesContainer';
+import { useSelector,useDispatch } from 'react-redux';
 
+
+const deposit = () => ({type: 'DEPOSIT'});
+const Withdrawal = () => ({type: 'WITHDRAWAL'});
 
 export default function Practice() {
+  const [ inputNum, setInputNum ] = useState('');
+  const dispatch = useDispatch();
+    const money = useSelector((state) => state.money.money);
     return (
         <div>
           <h1>React Redux</h1>
-          <h3>코딩온 은행</h3>
-          <MoneyContainer />
+        <h3>코딩온 은행</h3>
+        <h5>잔액: {money}</h5>
+        <input type='number' placeholder='금액을 입력하세요'
+        onChange={(e) => (setInputNum(e.target.value))}
+        value={inputNum}></input> 
+        <button onClick={dispatch(deposit(parseInt(inputNum)))}>입금</button>
+        <button onClick={dispatch(Withdrawal(parseInt(inputNum)))}>출금</button>
         </div>
       );
-}
-
-export const Money = () => {
-    // const [ inputNum, setInputNum ] = useState('');
-    return (
-    <>
-        <h1>React Redux</h1>
-        <h3>코딩온 은행</h3>
-        {/* <h5>잔액: {inputNum}</h5> */}
-        {/* <input type='number' placeholder='금액을 입력하세요' */}
-        {/* onChange={(e) => (setInputNum(e.target.value))} */}
-        {/* value={inputNum}></input> */}
-        {/* <button onClick={Deposit(inputNum)}>입금</button> */}
-        {/* <button onClick={Withdrawal(inputNum)}>출금</button> */}
-    </>
-    )
 }
